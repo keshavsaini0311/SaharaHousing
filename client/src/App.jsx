075981cfa29f4f135e8de266e99470074fc6f544
keshavsaini0311 <keshavsaini1234567890@gmail.com>
2024-03-28 16:46:38 +0530

    sign in page ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,18 +2,18 @@
 import React from 'react'
 import { BrowserRouter ,Routes,Route} from 'react-router-dom'
 import Home from './pages/Home'
-import SignI from './pages/SignI'
 import Signup from './pages/Signup'
 import About from './pages/About'
 import Profile from './pages/Profile'
 import Header from './components/Header'
+import Signin from './pages/Signin'
 
 export default function App() {
   return <BrowserRouter>
   <Header/>
   <Routes>
     <Route path='/' element={<Home />}/>
-    <Route path='/sign-in' element={<SignI />}/>
+    <Route path='/sign-in' element={<Signin />}/>
     <Route path='/sign-up' element={<Signup />}/>
     <Route path='/about' element={<About />}/>
     <Route path='/profile' element={<Profile />}/>
