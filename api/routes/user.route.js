9166fc8cd9317932d57ee9d0d30b484930b0e495
keshavsaini0311 <keshavsaini1234567890@gmail.com>
2024-04-14 18:52:00 +0530

    update user backend

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,8 +1,10 @@
 import express from 'express';
-import { test } from '../controllers/user.controller.js';
+import { test, updateUser } from '../controllers/user.controller.js';
+import { veryfyToken } from '../utils/verifyUSer.js';
 
 const router=express.Router();
 
 router.get('/test',test);
+router.post('/update/:id', veryfyToken,updateUser);
 
 export default router;
