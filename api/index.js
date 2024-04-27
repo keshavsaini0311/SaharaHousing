5f511780ac3053f186481d8ba06cb189260436aa
keshavsaini0311 <keshavsaini1234567890@gmail.com>
2024-04-28 03:30:06 +0530

    final touches for render

--- api/index.js
+++ api/index.js
@@ -5,6 +5,9 @@ import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js'
 import cookieParser from 'cookie-parser';
 import listingRouter from './routes/listing.route.js';
+import path from 'path';
+
+
 dotenv.config();
 
 mongoose.connect(process.env.mongo).then(()=>{
@@ -12,6 +15,8 @@ mongoose.connect(process.env.mongo).then(()=>{
 }).catch((err)=>{
     console.log(err);
 })
+
+const __dirname = path.resolve();
 
 const app=express();
 app.use(express.json());
@@ -25,6 +30,11 @@ app.use('/api/user',userRouter);
 app.use('/api/auth',authRouter);
 app.use('/api/listing',listingRouter);
 
+app.use(express.static(path.join(__dirname, 'client/dist')))
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client','dist','index.html'))
+})
 
 app.use((err,req,res,next)=>{
     const statuscode=err.statuscode || 500;
