8abab83e78999f2a4d7dc1d7d0986f6fbb02adfe
keshavsaini0311 <keshavsaini1234567890@gmail.com>
2024-07-14 22:28:56 +0530

    corrected website name

--- api/cron/cron.js
+++ api/cron/cron.js
@@ -1,7 +1,7 @@
 import cron from "cron";
 import https from "https";
 
-const URL = "https://social-media-9ll3.onrender.com/";
+const URL = "https://saharahousing.onrender.com/";
 
 const job = new cron.CronJob("*/5 * * * *", function () {
 	https
