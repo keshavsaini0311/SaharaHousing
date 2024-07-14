ca20dac1c2fd50cb22f2be7f4ce85cb7735f0c1e
keshavsaini0311 <keshavsaini1234567890@gmail.com>
2024-07-14 23:12:51 +0530

    solve new deploy bugs

--- api/index.js
+++ api/index.js
@@ -6,11 +6,12 @@ import authRouter from './routes/auth.route.js'
 import cookieParser from 'cookie-parser';
 import listingRouter from './routes/listing.route.js';
 import path from 'path';
-import cors from 'cors';
 import job from "./cron/cron.js";
 
 dotenv.config();
 job.start();
+
+const app=express();
 
 mongoose.connect(process.env.mongo).then(()=>{
     console.log("connected");
